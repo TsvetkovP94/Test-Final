ccf9fcae600eaa756ae72564880a05d60a2098c4
TsvetkovP94 <53179088+TsvetkovP94@users.noreply.github.com>
2019-08-01 12:21:45 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+settings = {
+    "http_server_port": 8001,
+    "db": {
+        "db": "webhooks",
+        "host": "127.0.0.1",
+        "port": 3306,
+        "user": "root",
+        "passwd": "root",
+        "provider": "mysql"
+    },
+}
